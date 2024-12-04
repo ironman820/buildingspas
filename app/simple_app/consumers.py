a805bd275ec726ad1dc17e6a92f2714a636163a9
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-12-04 22:13:05 +0000

    Dec 4, 2024, 10:13 PM

---
+++ app/simple_app/consumers.py
@@ -0,0 +1,16 @@
+from channels.generic.websocket import WebsocketConsumer
+
+
+class EchoConsumer(WebsocketConsumer):
+    def connect(self):
+        """Event when client connects"""
+
+        self.accept()
+        self.send(text_data="You are connected by Websockets!")
+
+    def disconnect(self, close_code):
+        """Event when client disconnects"""
+        pass
+
+    def receive(self, text_data=None, bytes_data=None):
+        return super().receive(text_data, bytes_data)
