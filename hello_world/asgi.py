a0a08b0adb9e54dc2d829739300316adcd475bf0
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-12-04 22:19:44 +0000

    Dec 4, 2024, 10:19 PM

--- hello_world/asgi.py
+++ hello_world/asgi.py
@@ -9,8 +9,23 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.urls import re_path
+from app.simple_app.consumers import EchoConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_world.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello_world.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                [
+                    re_path(r"^ws/echo/$", EchoConsumer.as_asgi()),
+                ]
+            )
+        ),
+    }
+)
