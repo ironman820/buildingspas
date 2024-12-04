efae028ca28d01807cd2e81914d0ee21a90bac56
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-12-04 22:33:40 +0000

    Dec 4, 2024, 10:33 PM

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     "node_modules/htmx.org/dist",
 ]
 
